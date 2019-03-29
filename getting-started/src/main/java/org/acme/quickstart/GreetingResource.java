6112aa000bc60f33d8d000e1640a0efa8bd9644c
sky0621 <sky14work@gmail.com>
2019-03-30 02:57:56 +0900

    continue

--- getting-started/src/main/java/org/acme/quickstart/GreetingResource.java
+++ getting-started/src/main/java/org/acme/quickstart/GreetingResource.java
@@ -1,16 +1,39 @@
 package org.acme.quickstart;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService service;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return service.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greet2")
+    public CompletionStage<String> greet2() {
+        return CompletableFuture.supplyAsync(() -> {
+            return "HELLOOOOOO!";
+        });
     }
 }
